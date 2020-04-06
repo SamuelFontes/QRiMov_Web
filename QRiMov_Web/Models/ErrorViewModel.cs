2f4e0ce2ba06e0a81271f00ea561d22c7e744626
Samuel Hilario Dias Fontes <samuguel@hotmail.com.br>
2020-04-06 09:26:59 -0300

    Criando Projeto...

---
+++ QRiMov_Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QRiMov_Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
