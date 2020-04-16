1fce0e2fa60b006f763dfdc5f0067659e5315ace
Samuel Hilario Dias Fontes <samuguel@hotmail.com.br>
2020-04-16 09:24:00 -0300

    Conexao do AddDbContext criada

--- QRiMov_Web/Startup.cs
+++ QRiMov_Web/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using QRiMov_Web.Context;
 
 namespace QRiMov_Web
 {
@@ -23,6 +25,11 @@ namespace QRiMov_Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+
+
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
