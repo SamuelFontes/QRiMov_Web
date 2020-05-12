40000b656361f22559603ed3a12effa5dbec30ac
samguel <samuguel@hotmail.com.br>
2020-05-12 12:59:31 -0300

    Conexao criada

---
+++ QRiMov/QRiMov/Controllers/Conexao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace QRiMov.Controllers
+{
+    public class Conexao
+    {
+        private static readonly string conexao = @"Data Source=ccstecno.ddns.net;Initial Catalog=QRiMov;Persist Security Info=True;User ID=qrimov;Password=123@biscoito";
+        private static SqlConnection conn = null;
+
+        public static SqlConnection abrirConexao()
+        {
+            conn = new SqlConnection(conexao);
+            try
+            {
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                conn = null;
+                Console.Write(ex);
+            }
+            return conn;
+        }
+        public static void fechaConexao()
+        {
+            if (conn != null)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
--- QRiMov/QRiMov/Models/UsuarioDAL.cs
+++ QRiMov/QRiMov/Models/UsuarioDAL.cs
@@ -1,19 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using QRiMov.Controllers;
 
 namespace QRiMov.Models
 {
+    
     public class UsuarioDAL : IUsuarioDAL
     {
+        private bool var = false;
+        SqlCommand cmd = new SqlCommand();
+        Conexao conexao = new Conexao();
+        SqlConnection conn = Conexao.abrirConexao();
+        SqlDataReader dr;
+        public void AddUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteUsuario(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
         public IEnumerable<Usuario> GetAllUsuarios()
         {
+            List<Usuario> lstUsuarios = new List<Usuario>();
 
+        }
+
+        public Usuario GetUsuario(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
         }
     }
 }
