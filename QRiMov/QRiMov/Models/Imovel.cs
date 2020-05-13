dc8ed494843b144004cc95dd98c3e6e41d9bf950
Samuel Hilario Dias Fontes <samuguel@hotmail.com.br>
2020-05-13 10:50:39 -0300

    Classe de imovel criada

---
+++ QRiMov/QRiMov/Models/Imovel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRiMov.Models
+{
+    public class Imovel
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public string Valor { get; set; }
+        [Required]
+        public string CEP { get; set; }
+        [Required]
+        public string Logradouro { get; set; }
+        [Required]
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        [Required]
+        public string Bairro { get; set; }
+        [Required]
+        public string UF { get; set; }
+        [Required]
+        public string Comarca { get; set; }
+    }
+}
