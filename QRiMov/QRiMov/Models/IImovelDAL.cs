cddb53dd4722782bdf2cf7d1ea7a927129d019b6
Samuel Hilario Dias Fontes <samuguel@hotmail.com.br>
2020-05-13 13:22:09 -0300

    Criada interface do Imovel

---
+++ QRiMov/QRiMov/Models/IImovelDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRiMov.Models
+{
+    interface IImovelDAL
+    {
+        IEnumerable<Imovel> GetAllImoveis();
+        void AddImovel(Imovel imovel);
+        void UpdateImovel(Imovel imovel);
+        Imovel GetImovel(int? id);
+        void DeleteImovel(int? id);
+    }
+}
