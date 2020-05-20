50941e76b66a428619069f87a26ac71797105e8e
samguel <samuguel@hotmail.com.br>
2020-05-20 09:16:54 -0300

    Ajusetes na view

---
+++ QRiMovWeb/QRiMovWeb/Controllers/ImovelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using QRiMovWeb.Repoositories;
+
+namespace QRiMovWeb.Controllers
+{
+    public class ImovelController : Controller
+    {
+        private readonly IImovelRepository _imovelRepository;
+        private readonly ICategoriaRepository _categoriaRepository;
+
+        public ImovelController(IImovelRepository imovelRepository,ICategoriaRepository categoriaRepository)
+        {
+            _imovelRepository = imovelRepository;
+            _categoriaRepository = categoriaRepository;
+        }
+
+        public IActionResult List()
+        {
+            ViewBag.Imovel = "Imovel";
+            ViewData["Categoria"] = "Categoria";
+            var imoveis = _imovelRepository.Imoveis;
+            return View(imoveis);
+        }
+    }
+}
