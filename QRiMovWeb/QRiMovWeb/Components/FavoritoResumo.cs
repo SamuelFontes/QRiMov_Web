5bf9bc7f4886c08d27d4aa5b7f5f90c1cdd85002
samguel <samuguel@hotmail.com.br>
2020-05-20 12:12:50 -0300

    Contato

--- QRiMovWeb/QRiMovWeb/Components/FavoritoResumo.cs
+++ QRiMovWeb/QRiMovWeb/Components/FavoritoResumo.cs
@@ -1,11 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using QRiMovWeb.Models;
+using QRiMovWeb.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace QRiMovWeb.Components
 {
-    public class FavoritoResumo
+    public class FavoritoResumo : ViewComponent
     {
+        private readonly Favorito _favorito;
+
+        public FavoritoResumo(Favorito favorito)
+        {
+            _favorito = favorito;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var itens = _favorito.GetFavoritoItems();
+            _favorito.FavoritoItems = itens;
+            var favoritoVM = new FavoritoViewModel
+            {
+                Favorito = _favorito,
+                FavoritoTotal = _favorito.GetFavoritoTotal()
+            };
+            return View(favoritoVM);
+        }
     }
 }
