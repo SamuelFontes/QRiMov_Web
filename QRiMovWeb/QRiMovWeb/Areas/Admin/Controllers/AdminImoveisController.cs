b946d3ad9ddc9cbc31a8a7e21724bf7a03750d1d
samguel <samuguel@hotmail.com.br>
2020-05-20 18:56:12 -0300

    Criado telas na area admin

---
+++ QRiMovWeb/QRiMovWeb/Areas/Admin/Controllers/AdminImoveisController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QRiMovWeb.Context;
+using QRiMovWeb.Models;
+
+namespace QRiMovWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class AdminImoveisController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AdminImoveisController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Admin/AdminImoveis
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Imoveis.Include(i => i.Categoria);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Admin/AdminImoveis/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imovel = await _context.Imoveis
+                .Include(i => i.Categoria)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (imovel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imovel);
+        }
+
+        // GET: Admin/AdminImoveis/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaId");
+            return View();
+        }
+
+        // POST: Admin/AdminImoveis/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Descricao,Valor,CEP,Logradouro,Numero,Complemento,Bairro,UF,Comarca,ImgMiniaturaUrl,ImgUrl,IsAtivo,IsDestaque,CategoriaId")] Imovel imovel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(imovel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaId", imovel.CategoriaId);
+            return View(imovel);
+        }
+
+        // GET: Admin/AdminImoveis/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imovel = await _context.Imoveis.SingleOrDefaultAsync(m => m.Id == id);
+            if (imovel == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaId", imovel.CategoriaId);
+            return View(imovel);
+        }
+
+        // POST: Admin/AdminImoveis/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Descricao,Valor,CEP,Logradouro,Numero,Complemento,Bairro,UF,Comarca,ImgMiniaturaUrl,ImgUrl,IsAtivo,IsDestaque,CategoriaId")] Imovel imovel)
+        {
+            if (id != imovel.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(imovel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ImovelExists(imovel.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categorias, "CategoriaId", "CategoriaId", imovel.CategoriaId);
+            return View(imovel);
+        }
+
+        // GET: Admin/AdminImoveis/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var imovel = await _context.Imoveis
+                .Include(i => i.Categoria)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (imovel == null)
+            {
+                return NotFound();
+            }
+
+            return View(imovel);
+        }
+
+        // POST: Admin/AdminImoveis/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var imovel = await _context.Imoveis.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Imoveis.Remove(imovel);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ImovelExists(int id)
+        {
+            return _context.Imoveis.Any(e => e.Id == id);
+        }
+    }
+}
