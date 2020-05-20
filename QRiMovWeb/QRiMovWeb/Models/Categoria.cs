c339af29cafb2ace5437e70143fdf683d27f2380
samguel <samuguel@hotmail.com.br>
2020-05-20 07:12:33 -0300

    Classes Criadas

---
+++ QRiMovWeb/QRiMovWeb/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QRiMovWeb.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        public List<Imovel> Imoveis { get; set; }
+    }
+}
---
+++ QRiMovWeb/QRiMovWeb/Models/Imovel.cs
@@ -0,0 +1,22 @@
+﻿namespace QRiMovWeb.Models
+{
+    public class Imovel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public string Valor { get; set; }
+        public string CEP { get; set; }
+        public string Logradouro { get; set; }
+        public string Numero { get; set; }
+        public string Complemento { get; set; }
+        public string Bairro { get; set; }
+        public string UF { get; set; }
+        public string Comarca { get; set; }
+        public string ImgMiniaturaUrl { get; set; }
+        public string ImgUrl { get; set; }
+        public bool IsAtivo { get; set; }
+        public bool IsDestaque { get; set; }
+        public int CategoriaId { get; set; }
+        public virtual Categoria Categoria { get; set; }
+    }
+}
