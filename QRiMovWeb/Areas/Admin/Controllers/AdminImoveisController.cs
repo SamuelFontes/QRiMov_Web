7b3045b03aab338fb1e7127bc6797d9de4752495
samguel <samuguel@hotmail.com.br>
2020-06-06 17:53:31 -0300

    qr code gerando link

--- QRiMovWeb/Areas/Admin/Controllers/AdminImoveisController.cs
+++ QRiMovWeb/Areas/Admin/Controllers/AdminImoveisController.cs
@@ -65,7 +65,7 @@ namespace QRiMovWeb.Areas.Admin.Controllers
             {
                 _context.Add(imovel);
                 await _context.SaveChangesAsync();
-                string ImgQRCode = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=samuguel-001-site1.gtempurl.com/" + imovel.ImovelId.ToString();
+                string ImgQRCode = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=samuguel-001-site1.gtempurl.com/Imovel/Details?ImovelId=" + imovel.ImovelId.ToString();
                 _context.Database.ExecuteSqlCommand("UPDATE IMOVEIS SET ImgQRCode = '" + ImgQRCode + "' where imovelID = " + imovel.ImovelId.ToString());
                 return RedirectToAction(nameof(Index));
             }
